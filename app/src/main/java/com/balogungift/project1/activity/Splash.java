a25f94d36d80a998b75f9b606395f07915c09d8f
Gift Balogun <blgnbalogun52@gmail.com>
2019-04-01 16:33:26 +0100

    second commit

---
+++ app/src/main/java/com/balogungift/project1/activity/Splash.java
@@ -0,0 +1,16 @@
+package com.balogungift.project1.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.balogungift.project1.R;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/balogungift/project1/context/ApplicationContext.java
@@ -0,0 +1,45 @@
+package com.balogungift.project1.context;
+
+import android.app.Application;
+
+import com.balogungift.project1.bean.AttendanceBean;
+import com.balogungift.project1.bean.AttendanceSessionBean;
+import com.balogungift.project1.bean.FacultyBean;
+import com.balogungift.project1.bean.StudentBean;
+
+import java.util.ArrayList;
+
+public class ApplicationContext extends Application {
+	private FacultyBean facultyBean;
+	private AttendanceSessionBean attendanceSessionBean;
+	private ArrayList<StudentBean> studentBeanList;
+	private ArrayList<AttendanceBean> attendanceBeanList;
+	
+	public FacultyBean getFacultyBean() {
+		return facultyBean;
+	}
+	public void setFacultyBean(FacultyBean facultyBean) {
+		this.facultyBean = facultyBean;
+	}
+	public AttendanceSessionBean getAttendanceSessionBean() {
+		return attendanceSessionBean;
+	}
+	public void setAttendanceSessionBean(AttendanceSessionBean attendanceSessionBean) {
+		this.attendanceSessionBean = attendanceSessionBean;
+	}
+	public ArrayList<StudentBean> getStudentBeanList() {
+		return studentBeanList;
+	}
+	public void setStudentBeanList(ArrayList<StudentBean> studentBeanList) {
+		this.studentBeanList = studentBeanList;
+	}
+	public ArrayList<AttendanceBean> getAttendanceBeanList() {
+		return attendanceBeanList;
+	}
+	public void setAttendanceBeanList(ArrayList<AttendanceBean> attendanceBeanList) {
+		this.attendanceBeanList = attendanceBeanList;
+	}
+	
+	
+
+}
